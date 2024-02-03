db3215c9d1ff9e05413f8060c87aeccf3b1c3730
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-03 15:00:50 +0100

    3-10-Refactor: Organize import statements

---
+++ app/components/index.ts
@@ -0,0 +1,17 @@
+// import Link from './Link';
+// import ErrorMessage from './ErrorMessage';
+// import IssueStatusBadge from './IssueStatusBadge';
+// import Spinner from './Spinner';
+
+// export { Link }; // {} mean it'll be a named export
+// export {ErrorMessage};
+// export {IssueStatusBadge};
+// export {Spinner};
+
+// OR
+// in one go:
+export { default as Link } from './Link'; // export default obj from this './Link' as Link;
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as IssueStatusBadge } from './IssueStatusBadge';
+export { default as Spinner } from './Spinner';
+export { default as Skeleton } from './Skeleton'
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,6 @@
-import React from "react";
 import { Table } from "@radix-ui/themes";
-import Link from "../components/Link";
-
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+import { IssueStatusBadge, Link } from "@/app/components";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
