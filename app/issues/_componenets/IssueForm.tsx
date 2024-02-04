929580e6f8bf35af41c1a96891719ebaf6939ce9
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-04 11:00:47 +0100

    4-5-Update issues

--- app/issues/_componenets/IssueForm.tsx
+++ app/issues/_componenets/IssueForm.tsx
@@ -41,7 +41,8 @@ const IssueForm = ({ issue }: Props) => {
     try {
       setSubmitting(true);
       //   createIssue(data) - if separate axios into a func
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setSubmitting(false);
@@ -77,7 +78,8 @@ const IssueForm = ({ issue }: Props) => {
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
