51c87c85520b3c18e66127560bf1e82bc330e3d9
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-04 07:43:22 +0100

    4-2-Refactor: Apply the SRP-2

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,6 @@
-import React from "react";
 import prisma from "@/prisma/client";
+import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import ReactMarkdown from "react-markdown";
-import delay from "delay";
-import Link from "next/link";
-import { Pencil2Icon } from "@radix-ui/react-icons";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 
@@ -23,7 +17,6 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   if (!issue) return notFound();
 
-  await delay(2000);
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
