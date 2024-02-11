b3d17c20ea684aa0a8eafacde29430f8b41d1ccd
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-11 13:06:03 +0100

    8-10-Refactor: Extract IssueTable

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -6,7 +6,7 @@ import IssueStatusFilter from "./IssueStatusFilter";
 const IssueActions = () => {
   return (
     <div>
-      <Flex mb="5" justify="between">
+      <Flex justify="between">
         <IssueStatusFilter />
         <Button>
           <Link href="/issues/new/">New Issue Page</Link>
