857bf9558293d030ba307a92f539025c961da560
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-11 07:47:48 +0100

    7-4-Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -11,16 +11,6 @@ interface Props {
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
-
-  const issues = await prisma.issue.findMany({
-    where: { status: status },
-  });
-  // await delay(2000);
-
   const columns: {
     label: string;
     value: keyof Issue;
@@ -30,6 +20,23 @@ const IssuesPage = async ({ searchParams }: Props) => {
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status: status },
+    orderBy,
+  });
+  // await delay(2000);
 
   return (
     <div>
